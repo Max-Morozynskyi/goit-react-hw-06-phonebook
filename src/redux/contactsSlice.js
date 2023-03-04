8029509373859f20870d4f3b69a1715c17a44d3f
Max-Morozynskyi <m0r0zin@ukr.net>
2023-03-04 19:47:48 +0200

    persister added

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,11 +1,20 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const initialContacts = [
-  { id: 'id-1', name: 'Bart Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Weasley', number: '443-89-12' },
-  { id: 'id-3', name: 'Eric Cartman', number: '645-17-79' },
-  { id: 'id-4', name: 'Porkey Pig', number: '227-91-26' },
-];
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+const initialContacts = {
+  contacts: [
+    { id: 'id-1', name: 'Bart Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Weasley', number: '443-89-12' },
+    { id: 'id-3', name: 'Eric Cartman', number: '645-17-79' },
+    { id: 'id-4', name: 'Porkey Pig', number: '227-91-26' },
+  ]
+};
 
 const contactsSlice = createSlice({
   name: 'contacts',
@@ -13,7 +22,7 @@ const contactsSlice = createSlice({
   reducers: {
     createContact: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.contacts.push(action.payload);
       },
       prepare({ name, number }) {
         return {
@@ -26,10 +35,10 @@ const contactsSlice = createSlice({
       }
     },
     deleteContact(state, action) {
-      return state.filter(contact => contact.id !== action.payload)
+      state.contacts = state.contacts.filter(contact => contact.id !== action.payload)
     }
   }
 })
 
 export const { createContact, deleteContact } = contactsSlice.actions;
-export const contactsReducer = contactsSlice.reducer;
+export const contactsReducer = persistReducer(persistConfig, contactsSlice.reducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, contactsReducer);
-
 export const store = configureStore({
   reducer: {
-    contacts: persistedReducer,
+    contacts: contactsReducer,
     filter: filterReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 
 });
 
